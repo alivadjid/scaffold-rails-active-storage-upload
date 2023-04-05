ba42ec7f38bbef386fe4793ce7cc9dba6037dba0
alivadjid <snub01@mail.ru>
2023-04-05 23:51:49 +0300

    react components update

---
+++ frontend/src/components/FileForm.jsx
@@ -0,0 +1,44 @@
+import { AppContext } from "../App";
+import React, { useContext } from "react";
+
+function FileForm() {
+  const { latestPost, setLatestPost } = useContext(AppContext);
+
+  function handleSubmit(event) {
+    event.preventDefault();
+
+    const data = new FormData();
+
+    data.append("post[title]", event.target.title.value);
+    data.append("post[image]", event.target.image.files[0]);
+  }
+
+  function submitToAPI(data) {
+    fetch("https://localhost:3000/posts", {
+      method: "POST",
+      body: data,
+    })
+      .then((res) => res.json())
+      .then((data) => setLatestPost(data.image_url))
+      .catch((err) => console.error(err));
+  }
+
+  return (
+    <div>
+      <h1>File Form</h1>
+      <form onSubmit={(e) => handleSubmit(e)}>
+        <label htmlFor="title">Title</label>
+        <input type="text" name="title" id="title" />
+        <br />
+
+        <label htmlFor="image">Image</label>
+        <input type="file" name="image" id="image" />
+        <br />
+
+        <button type="submit">Create post</button>
+      </form>
+    </div>
+  );
+}
+
+export default FileForm;
---
+++ frontend/src/components/LatestImage.jsx
@@ -0,0 +1,21 @@
+import { AppContext } from "../App";
+import React, { useContext, useEffect } from "react";
+
+function LatestImage() {
+  const { latestPost, setLatestPost } = useContext(AppContext);
+
+  useEffect(() => {
+    fetch("http://localhost:3000/posts")
+      .then((res) => res.json())
+      .then((data) => setLatestPost(data.image_url))
+      .catch((err) => console.error(err));
+  }, [latestPost]);
+
+  return (
+    <div>
+      <img src={latestPost} alt="latest post" className="latest-image" />
+    </div>
+  );
+}
+
+export default LatestImage;
