9bb060730717402afcce55a794d0803651c4693d
alivadjid <snub01@mail.ru>
2023-04-06 00:16:49 +0300

    change cors

--- frontend/src/components/LatestImage.jsx
+++ frontend/src/components/LatestImage.jsx
@@ -5,7 +5,7 @@ function LatestImage() {
   const { latestPost, setLatestPost } = useContext(AppContext);
 
   useEffect(() => {
-    fetch("http://localhost:3000/posts")
+    fetch("http://localhost:3000/latest")
       .then((res) => res.json())
       .then((data) => setLatestPost(data.image_url))
       .catch((err) => console.error(err));
